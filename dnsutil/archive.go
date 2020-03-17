8d76b8a5f5e62f6a4ddd64059e1ef05550f5fb1f
Luis Guillen Civera <luisguillenc@gmail.com>
2020-03-17 09:15:33 +0100

    changed dnsutil.ResolvData fields

--- dnsutil/archive.go
+++ dnsutil/archive.go
@@ -13,12 +13,19 @@ type Archiver interface {
 	SaveResolv(context.Context, ResolvData) (string, error)
 }
 
-// ResolvData defines struct for archive
+// ResolvData defines struct for summary ip resolutions
 type ResolvData struct {
-	ID        string    `json:"id" bson:"_id"`
-	Timestamp time.Time `json:"timestamp"`
-	Server    net.IP    `json:"server"`
-	Client    net.IP    `json:"client"`
-	Resolved  []net.IP  `json:"resolved"`
-	Name      string    `json:"name"`
+	ID        string        `json:"id" bson:"_id"`
+	Timestamp time.Time     `json:"timestamp"`
+	Duration  time.Duration `json:"duration"`
+	Server    net.IP        `json:"server"`
+	Client    net.IP        `json:"client"`
+	//query info
+	QID              uint16 `json:"qid"`
+	Name             string `json:"name"`
+	CheckingDisabled bool   `json:"checkingDisabled"`
+	//response info
+	ReturnCode        uint16   `json:"returnCode"`
+	AuthenticatedData bool     `json:"authenticatedData"`
+	Resolved          []net.IP `json:"resolved,omitempty" bson:",omitempty"`
 }
